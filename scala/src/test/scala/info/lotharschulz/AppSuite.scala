e2449a26ef8400891cfa9a1c4d63812e52fcf209
Lothar Schulz <lothar.sch@gmx.de>
2016-05-19 23:54:15 +0200

    comment dropped

--- scala/src/test/scala/info/lotharschulz/AppSuite.scala
+++ scala/src/test/scala/info/lotharschulz/AppSuite.scala
@@ -5,27 +5,11 @@ import org.scalatest.WordSpec
 class AppSuite extends WordSpec {
 
   "ArmstrongNumbers instance" when {
-    "method generateDummyArmstrongNumbers is called" should{
-      "produce a list like" in{
-        val aNmbrs : ArmstrongNumbers = new ArmstrongNumbers();
-        assert(List(2, 4) === aNmbrs.generateDummyArmstrongNumbers(1,2))
+    "method generateDummyArmstrongNumbers is called" should {
+      "produce a list like" in {
+        val aNmbrs: ArmstrongNumbers = new ArmstrongNumbers();
+        assert(List(2, 4) === aNmbrs.generateDummyArmstrongNumbers(1, 2))
       }
     }
   }
-
-/*
-  "2" when {
-    "2 as well" should {
-      "be equal" in {
-        assert(2 === 2)
-      }
-
-      "produce NoSuchElementException when head is invoked" in {
-        intercept[NoSuchElementException] {
-          Set.empty.head
-        }
-      }
-    }
-  }
-*/
 }
