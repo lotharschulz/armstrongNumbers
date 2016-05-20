24ec422c3312de5f9cf54de13481ee7726ffc70e
Lothar Schulz <lothar.sch@gmx.de>
2016-05-20 23:54:09 +0200

    getNumberOfDigits and FlatSpec tests

--- scala/src/main/scala/info/lotharschulz/App.scala
+++ scala/src/main/scala/info/lotharschulz/App.scala
@@ -1,5 +1,7 @@
 package info.lotharschulz
 
+import scala.math.log10;
+
 object App {
   def main(args: Array[String]) =
     println("Hello World!")
@@ -8,6 +10,11 @@ object App {
 class ArmstrongNumbers() {
   def generateDummyArmstrongNumbers(start: Int, end: Int): List[Int] = {
     List(start+1, end+2)
+  }
+
+  def getNumberOfDigits(number: Int): Int = number match {
+    case 0 => 1
+    case _ => (log10(number) + 1).toInt
   }
 }
 
--- scala/src/test/scala/info/lotharschulz/AppSuite.scala
+++ scala/src/test/scala/info/lotharschulz/AppSuite.scala
@@ -1,15 +1,23 @@
 package info.lotharschulz
 
-import org.scalatest.WordSpec
+import org.scalatest.FlatSpec
+import org.scalatest.BeforeAndAfter
 
-class AppSuite extends WordSpec {
+class AppSuite extends FlatSpec with BeforeAndAfter {
 
-  "ArmstrongNumbers instance" when {
-    "method generateDummyArmstrongNumbers is called" should {
-      "produce a list like" in {
-        val aNmbrs: ArmstrongNumbers = new ArmstrongNumbers();
-        assert(List(2, 4) === aNmbrs.generateDummyArmstrongNumbers(1, 2))
-      }
-    }
+  var armstrongNumbers: ArmstrongNumbers = _
+
+  before {
+    armstrongNumbers = new ArmstrongNumbers
   }
+
+  "method generateDummyArmstrongNumbers is called" should "produce a list like" in {
+    assert(List(2, 4) === armstrongNumbers.generateDummyArmstrongNumbers(1, 2))
+  }
+
+  "getNumberOfDigits" should "produce the number of digits for the given parameter" in {
+    assert(3 === armstrongNumbers.getNumberOfDigits(100))
+    assert(1 === armstrongNumbers.getNumberOfDigits(0))
+  }
+
 }
