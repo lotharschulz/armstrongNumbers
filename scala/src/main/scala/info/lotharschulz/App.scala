8f17209fa35b4ca9a875e7c8f103ea39232ee449
Lothar Schulz <lothar.sch@gmx.de>
2016-05-22 00:18:08 +0200

    getDigits added

--- scala/src/main/scala/info/lotharschulz/App.scala
+++ scala/src/main/scala/info/lotharschulz/App.scala
@@ -1,6 +1,7 @@
 package info.lotharschulz
 
 import scala.math.log10;
+import scala.collection.mutable.ListBuffer
 
 object App {
   def main(args: Array[String]) =
@@ -16,5 +17,20 @@ class ArmstrongNumbers() {
     case 0 => 1
     case _ => (log10(number) + 1).toInt
   }
+
+  def getDigits(number: Int): List[Int] = number match {
+    case 0 => List(0)
+    case _ => {
+        var nmbr = number
+        var l = Nil
+        var d = new ListBuffer[Int]()
+        while (nmbr > 0) {
+          d += (nmbr % 10)
+          nmbr = nmbr / 10
+        }
+        d.toList.reverse
+      }
+    }
+
 }
 
--- scala/src/test/scala/info/lotharschulz/AppSuite.scala
+++ scala/src/test/scala/info/lotharschulz/AppSuite.scala
@@ -20,4 +20,10 @@ class AppSuite extends FlatSpec with BeforeAndAfter {
     assert(1 === armstrongNumbers.getNumberOfDigits(0))
   }
 
+  "getDigits" should "get digits from a number" in {
+    assert(List(1,0,0) === armstrongNumbers.getDigits(100))
+    assert(List(0) === armstrongNumbers.getDigits(0))
+    assert(List() === armstrongNumbers.getDigits(-1))
+  }
+
 }
