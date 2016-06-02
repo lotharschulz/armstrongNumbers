eda740912bbff4477b097ad72acc2e7a4c909ec4
Lothar Schulz <lothar.sch@gmx.de>
2016-06-03 00:26:57 +0200

    test restructuredd

--- python/armstrong/armstrong.py
+++ python/armstrong/armstrong.py
@@ -1,34 +1,47 @@
 import numbers
 
+
 class ArmstrongNumbers:
 
     def __init__(self):
         self.armstrongNumbers = []
 
     def generate_armstrong_numbers(self, begin=0, end=1):
-        if not isinstance(begin, numbers.Number):
-            raise ValueError('limit has to be a number')
-        elif not isinstance(end, numbers.Number):
-            raise ValueError('end has to be a number')
-        else:
-            self.armstrongNumbers = []
-            return [i for i in range(begin, end+1) if self.is_armstrong_number(i)]
+        self.check_number(begin)
+        self.check_number(end)
+        self.armstrongNumbers = []
+        return [i for i in range(begin, end+1) if self.is_armstrong_number(i)]
 
     def generate_armstrong_numbers_alt(self, begin, end):
-        if not isinstance(begin, numbers.Number):
-            raise ValueError('limit has to be a number')
-        elif not isinstance(end, numbers.Number):
-            raise ValueError('end has to be a number')
-        else:
-            generator = self.generate_armstrong_numbers_gen(begin, end)
-            return [x for x in generator if self.is_armstrong_number(x)]
+        self.check_number(begin)
+        self.check_number(end)
+        generator = self.generate_armstrong_numbers_gen(begin, end)
+        return [x for x in generator if self.is_armstrong_number(x)]
 
     def generate_armstrong_numbers_gen(self, begin, end):
         for i in range(begin, end+1):
             yield i
 
     def is_armstrong_number(self, number):
+        self.check_number(number)
         return False
+
+    def get_digits(self, number):
+        self.check_number(number)
+        digits = []
+        if number == 0:
+            return digits
+        while number > 0:
+            digits.append(number%10)
+            number = int(number / 10)
+        digits.reverse()
+        return digits
+
+    def check_number(self, number):
+        if not isinstance(number, numbers.Number):
+            raise ValueError('limit has to be a number')
+        elif number < 0:
+            raise ValueError('number must be greater than -1')
 
 if __name__ == '__main__':
     a = ArmstrongNumbers()
--- python/test/test_armstrong.py
+++ python/test/test_armstrong.py
@@ -15,15 +15,15 @@ class FixturesTest(unittest.TestCase):
 
     def test_generate_armstrong_numbers(self):
         self.assertEqual([], self.armstrongnumbers.generate_armstrong_numbers(100, 1000))
-
-    def test_generate_armstrong_numbers_error(self):
         self.assertRaises(ValueError, self.armstrongnumbers.generate_armstrong_numbers, "a", "b")
+        self.assertRaises(ValueError, self.armstrongnumbers.generate_armstrong_numbers, -1, 100)
+        self.assertRaises(ValueError, self.armstrongnumbers.generate_armstrong_numbers, 100, -1)
 
     def test_generate_armstrong_numbers_alt(self):
         self.assertEqual([], self.armstrongnumbers.generate_armstrong_numbers_alt(100, 1000))
-
-    def test_generate_armstrong_numbers_alt_error(self):
         self.assertRaises(ValueError, self.armstrongnumbers.generate_armstrong_numbers_alt, "a", "b")
+        self.assertRaises(ValueError, self.armstrongnumbers.generate_armstrong_numbers, -1, 100)
+        self.assertRaises(ValueError, self.armstrongnumbers.generate_armstrong_numbers, 100, -1)
 
     def test_generate_armstrong_numbers_gen(self):
         f = self.armstrongnumbers.generate_armstrong_numbers_gen(2,6)
@@ -34,6 +34,15 @@ class FixturesTest(unittest.TestCase):
 
     def test_is_armstrong_number(self):
         self.assertFalse(self.armstrongnumbers.is_armstrong_number(11))
+        self.assertFalse(self.armstrongnumbers.is_armstrong_number(0))
+        self.assertRaises(ValueError, self.armstrongnumbers.is_armstrong_number, "a")
+        self.assertRaises(ValueError, self.armstrongnumbers.is_armstrong_number, -1)
+
+    def test_get_digits(self):
+        self.assertEqual([1, 2, 3], self.armstrongnumbers.get_digits(123))
+        self.assertEqual([], self.armstrongnumbers.get_digits(0))
+        self.assertRaises(ValueError, self.armstrongnumbers.get_digits, "a")
+        self.assertRaises(ValueError, self.armstrongnumbers.get_digits, -1)
 
 if __name__ == '__main__':
     unittest.main()
