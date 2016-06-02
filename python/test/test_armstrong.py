5d34dbafe396ffdb9cab44267bb92e91b576a185
Lothar Schulz <lothar.sch@gmx.de>
2016-06-03 00:02:11 +0200

    ValueError check added

--- python/test/test_armstrong.py
+++ python/test/test_armstrong.py
@@ -1,4 +1,6 @@
-import unittest, os, sys
+import unittest
+import os
+import sys
 sys.path.append(os.path.abspath('..'))
 from armstrong.armstrong import ArmstrongNumbers
 
@@ -11,13 +13,17 @@ class FixturesTest(unittest.TestCase):
     def tearDown(self):
         del self.armstrongnumbers
 
-    # @TODO: test ValueErrors
     def test_generate_armstrong_numbers(self):
         self.assertEqual([], self.armstrongnumbers.generate_armstrong_numbers(100, 1000))
 
-    # @TODO: test ValueErrors
+    def test_generate_armstrong_numbers_error(self):
+        self.assertRaises(ValueError, self.armstrongnumbers.generate_armstrong_numbers, "a", "b")
+
     def test_generate_armstrong_numbers_alt(self):
         self.assertEqual([], self.armstrongnumbers.generate_armstrong_numbers_alt(100, 1000))
+
+    def test_generate_armstrong_numbers_alt_error(self):
+        self.assertRaises(ValueError, self.armstrongnumbers.generate_armstrong_numbers_alt, "a", "b")
 
     def test_generate_armstrong_numbers_gen(self):
         f = self.armstrongnumbers.generate_armstrong_numbers_gen(2,6)
