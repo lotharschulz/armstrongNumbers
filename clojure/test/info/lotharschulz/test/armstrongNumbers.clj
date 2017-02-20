6ed325a0df316869143632803fcc729da4c82c92
Lothar Schulz <lothar.sch@gmx.de>
2017-02-20 22:57:48 +0100

    neg tests

--- clojure/test/info/lotharschulz/test/armstrongNumbers.clj
+++ clojure/test/info/lotharschulz/test/armstrongNumbers.clj
@@ -5,8 +5,11 @@
 (facts "about getNumberOfDigits"
        (fact "return number of digits if given number greater then -1"
              (getNumberOfDigits [100]) => 3
+             (= 4 (getNumberOfDigits [10000])) => falsey
              (getNumberOfDigits [10000]) => 5
+             (= 4 (getNumberOfDigits [10000])) => falsey
              (getNumberOfDigits [0]) => 1
+             (= 4 (getNumberOfDigits [0])) => falsey
              (getNumberOfDigits [-1]) => nil
              (getNumberOfDigits ["a"]) => nil
              (getNumberOfDigits [1.0]) => nil
