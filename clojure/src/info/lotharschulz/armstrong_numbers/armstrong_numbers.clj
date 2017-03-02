396833099ffa0d27259a248a93ece2f5032fb49e
Lothar Schulz <lothar.sch@gmx.de>
2017-03-02 23:49:34 +0100

    some more feedback incorporated

--- clojure/src/info/lotharschulz/armstrong_numbers/armstrong_numbers.clj
+++ clojure/src/info/lotharschulz/armstrong_numbers/armstrong_numbers.clj
@@ -11,7 +11,7 @@
       nil)))
 
 (defn get-digits-alt [^Integer n]
-  {:pre [(not (nil? n)) (integer? n)]}
+  {:pre [(some? n) (integer? n)]}
   "return number of digits of given int n"
     (if (zero? n)
       [0]
@@ -20,13 +20,13 @@
         [])))
 
 (defn get-digits [^Integer n]
-  {:pre [(not (nil? n)) (integer? n)]}
+  {:pre [(some? n) (integer? n)]}
   "return number of digits of given int n"
   (when-not (zero? n)
     (lazy-seq (cons (rem n 10) (get-digits (quot n 10))))))
 
 (defn is-armstrong-number [^Integer n]
-  {:pre [(not (nil? n)) (integer? n)]}
+  {:pre [(some? n) (integer? n)]}
   "Returns true if the given number bigger than -1 is an armstrong number"
   (if (zero? n)
     false
@@ -34,8 +34,18 @@
       (= n (reduce + (map #(int (Math/pow % (get-number-of-digits n))) (get-digits n))))
       false)))
 
+(defn armstrong-number? [^Integer n]
+  {:pre [(some? n) (integer? n)]}
+  (and (pos? n)
+       (let [digits-of-n      (get-digits n)
+             number-of-digits (count digits-of-n)]
+         (== n (->> digits-of-n
+                    (map #(Math/pow % number-of-digits))
+                    (reduce +))))))
+
+
 (defn generate-armstrong-numbers [^Integer from, ^Integer to]
-  {:pre [(not (nil? from)) (integer? from) (pos? from) (not (nil? to)) (integer? to) (pos? to) ]}
+  {:pre [(some? from) (integer? from) (some? to) (integer? to) ]}
   "generate armstrong numbers for ranges of ints greater than 0 defined by from and to"
   (if (< from to)
     (into [] (for [x (range from to)
--- clojure/test/info/lotharschulz/armstrong_numbers/armstrong_numbers_test.clj
+++ clojure/test/info/lotharschulz/armstrong_numbers/armstrong_numbers_test.clj
@@ -17,15 +17,15 @@
              (get-number-of-digits ["a", 1]) => (throws AssertionError)
              )
        (fact "list of digits for given number"
-             (get-digits 100) => [1 0 0]
-             (get-digits 0) => [0]
-             (get-digits -1) => []
+             (get-digits 100) => [0 0 1]
+             (get-digits 0) => nil
+             (get-digits -1) => [-1]
              (get-digits nil) => (throws AssertionError)
              (get-digits "a") => (throws AssertionError)
              (get-digits 1.0) => (throws AssertionError)
              (get-digits ["a", 1]) => (throws AssertionError)
              )
-       (fact "list of digits for given number"
+       (fact "list of digits for given number (alt)"
              (get-digits-alt 100) => [1 0 0]
              (get-digits-alt 0) => [0]
              (get-digits-alt -1) => []
@@ -50,17 +50,34 @@
              (is-armstrong-number 1.0) => (throws AssertionError)
              (is-armstrong-number ["a", 1]) => (throws AssertionError)
              )
+       (fact "armstrong number check (?) for 153, 370, 371, 407"
+             (armstrong-number? 153) => truthy
+             (armstrong-number? 370) => truthy
+             (armstrong-number? 371) => truthy
+             (armstrong-number? 407) => truthy
+             (armstrong-number? 154) => falsey
+             (armstrong-number? 369) => falsey
+             (armstrong-number? 372) => falsey
+             (armstrong-number? 406) => falsey
+             (armstrong-number? 0) => falsey
+             (armstrong-number? -1) => falsey
+             (armstrong-number? nil) => (throws AssertionError)
+             (armstrong-number? "a") => (throws AssertionError)
+             (armstrong-number? 1.0) => (throws AssertionError)
+             (armstrong-number? ["a", 1]) => (throws AssertionError)
+             )
        (fact "generate armstrong numbers from 100 to 1000"
-             (generate-armstrong-numbers 100 1000) => [153 370 371 407])
-             (generate-armstrong-numbers 0 1000) => (throws AssertionError)
-             (generate-armstrong-numbers 1000 0) => (throws AssertionError)
+             (generate-armstrong-numbers 100 1000) => [153 370 371 407]
+             (generate-armstrong-numbers 1 1000) => [1 2 3 4 5 6 7 8 9 153 370 371 407]
+             (generate-armstrong-numbers 0 1000) => [1 2 3 4 5 6 7 8 9 153 370 371 407]
+             (generate-armstrong-numbers 1000 0) => []
              (generate-armstrong-numbers 2 2) => []
-             (generate-armstrong-numbers -1 1000) => (throws AssertionError)
-             (generate-armstrong-numbers 1000 -1) => (throws AssertionError)
-             (generate-armstrong-numbers -2 -2) => (throws AssertionError)
+             (generate-armstrong-numbers -1 1000) => [1 2 3 4 5 6 7 8 9 153 370 371 407]
+             (generate-armstrong-numbers 1000 -1) => []
+             (generate-armstrong-numbers -2 -2) => []
              (generate-armstrong-numbers nil nil) => (throws AssertionError)
              (generate-armstrong-numbers "a" "b") => (throws AssertionError)
              (generate-armstrong-numbers 1.0 2.0) => (throws AssertionError)
-             (generate-armstrong-numbers ["a", 1] ["b", 2]) => (throws AssertionError)
-       )
-       
+             (generate-armstrong-numbers ["a", 1] ["b", 2]) => (throws AssertionError))
+
+)
