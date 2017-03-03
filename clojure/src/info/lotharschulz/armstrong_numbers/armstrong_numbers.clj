d2bfffaf47c2470c681e8e22ad937d194a00f42f
lotharschulz <lothar.schulz@zalando.de>
2017-03-03 23:09:00 +0100

    incorporated PR comment about docstring

--- clojure/src/info/lotharschulz/armstrong_numbers/armstrong_numbers.clj
+++ clojure/src/info/lotharschulz/armstrong_numbers/armstrong_numbers.clj
@@ -1,40 +1,47 @@
 (ns info.lotharschulz.armstrong-numbers.armstrong-numbers
   (:import [java.Math]))
 
-(defn get-number-of-digits [^Integer n]
-  {:pre [(not (nil? n)) (integer? n)]}
+
+(defn get-number-of-digits
   "Returns the :getNumberOfDigits of n if n is an integer and greater than -1"
+  [^Integer n]
+  {:pre [(not (nil? n)) (integer? n)]}
   (if (zero? n)
     1
     (if (pos? n )
       (inc (int (Math/log10 n )))
       nil)))
 
-(defn get-digits-alt [^Integer n]
-  {:pre [(some? n) (integer? n)]}
+(defn get-digits-alt
   "return number of digits of given int n"
+  [^Integer n]
+  {:pre [(some? n) (integer? n)]}
     (if (zero? n)
       [0]
       (if (pos? n)
         (into [] (map #(Character/getNumericValue %) (str n)))
         [])))
 
-(defn get-digits [^Integer n]
-  {:pre [(some? n) (integer? n)]}
+(defn get-digits
   "return number of digits of given int n"
+  [^Integer n]
+  {:pre [(some? n) (integer? n)]}
   (when-not (zero? n)
     (lazy-seq (cons (rem n 10) (get-digits (quot n 10))))))
 
-(defn is-armstrong-number [^Integer n]
-  {:pre [(some? n) (integer? n)]}
+(defn is-armstrong-number
   "Returns true if the given number bigger than -1 is an armstrong number"
+  [^Integer n]
+  {:pre [(some? n) (integer? n)]}
   (if (zero? n)
     false
     (if (pos? n)
       (= n (reduce + (map #(int (Math/pow % (get-number-of-digits n))) (get-digits n))))
       false)))
 
-(defn armstrong-number? [^Integer n]
+(defn armstrong-number?
+  "Returns true if the given number is an armstrong number"
+  [^Integer n]
   {:pre [(some? n) (integer? n)]}
   (and (pos? n)
        (let [digits-of-n      (get-digits n)
@@ -44,9 +51,10 @@
                     (reduce +))))))
 
 
-(defn generate-armstrong-numbers [^Integer from, ^Integer to]
-  {:pre [(some? from) (integer? from) (some? to) (integer? to) ]}
+(defn generate-armstrong-numbers
   "generate armstrong numbers for ranges of ints greater than 0 defined by from and to"
+  [^Integer from, ^Integer to]
+  {:pre [(some? from) (integer? from) (some? to) (integer? to) ]}
   (if (< from to)
     (into [] (for [x (range from to)
                    :when (true? (is-armstrong-number x))]
