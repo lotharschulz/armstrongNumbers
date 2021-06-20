65181683015635f7fc99789100e1e5d58d38c905
Lothar Schulz <lothar.sch@gmx.de>
2021-06-21 00:01:11 +0200

    chore getDigits implementations

--- kotlin/src/main/kotlin/Armstrong.kt
+++ kotlin/src/main/kotlin/Armstrong.kt
@@ -1,3 +1,42 @@
 object Armstrong {
     fun isArmstrongNumber(input: Int): Boolean = true
+
+    fun getDigits(input: Int, acc: MutableList<Int>): List<Int> =
+        when {
+            input > 0 -> {
+                acc.add(input%10)
+                getDigits(input / 10, acc)
+            }
+            else -> {
+                acc.reverse()
+                acc
+            }
+        }
+
+    fun getDigits2(input: Int): List<Int> {
+        val result = mutableListOf<Int>()
+        var nmbr = input
+        if(nmbr>0){
+            while (nmbr > 0) {
+                result.add(input % 10);
+                nmbr = nmbr / 10;
+            }
+            result.reverse()
+        } else if (input == 0) {
+            result.add(0);
+        }
+        return result
+    }
+
+    fun getDigits3(input: Int): List<Int> {
+        val digits = ArrayList<Int>()
+        if (input >= 0) {
+            val inputString: String = input.toString()
+            inputString.forEach {
+                // https://www.lotharschulz.info/2018/08/05/kotlin-convert-string-containing-only-numbers-to-int/
+                digits.add(it.code - 48)
+            }
+        }
+        return digits
+    }
 }
--- kotlin/src/test/kotlin/ArmstrongTest.kt
+++ kotlin/src/test/kotlin/ArmstrongTest.kt
@@ -1,10 +1,40 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+
 class ArmstrongTest {
 
     @Test
     fun `5 is armstrong number`(){
         assertEquals(true, Armstrong.isArmstrongNumber(5))
+    }
+
+    @Test
+    fun `getDigits`(){
+        val digits1 = listOf(1,1,1,1)
+        val result1 = mutableListOf<Int>()
+        Armstrong.getDigits(1111, result1)
+        assertEquals(digits1, result1.toList())
+        val digits2 = listOf<Int>()
+        val result2 = mutableListOf<Int>()
+        Armstrong.getDigits(0, result2)
+        assertEquals(digits2, result2.toList())
+        val result3 = mutableListOf<Int>()
+        Armstrong.getDigits(-1, result3)
+        assertEquals(digits2, result3.toList())
+    }
+
+    @Test
+    fun `getDigits2`(){
+        assertEquals(listOf(1,1,1,1), Armstrong.getDigits2(1111))
+        assertEquals(listOf<Int>(0), Armstrong.getDigits2(0))
+        assertEquals(listOf<Int>(), Armstrong.getDigits2(-1))
+    }
+
+    @Test
+    fun `getDigits3`(){
+        assertEquals(listOf(1,1,1,1), Armstrong.getDigits3(1111))
+        assertEquals(listOf<Int>(0), Armstrong.getDigits3(0))
+        assertEquals(listOf<Int>(), Armstrong.getDigits3(-1))
     }
 }
