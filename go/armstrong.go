3ea9b560756bf6a79d43d64d11fc8d60ecb9900a
Lothar Schulz <lothar.sch@gmx.de>
2023-10-19 10:49:57 +0200

    chore test added for 3 digit armstrong number test fails by purpose at the moment, implementation is still a TODO

--- go/armstrong.go
+++ go/armstrong.go
@@ -1,5 +1,15 @@
 package armstrong
 
-func GenerateArmstrongNumbers() int {
-	return 123
+import (
+	"container/list"
+)
+
+func GenerateArmstrongNumbers() *list.List {
+	expectedList := list.New()
+	// expectedList.PushBack(153)
+	// expectedList.PushBack(370)
+	// expectedList.PushBack(371)
+	// expectedList.PushBack(407)
+
+	return expectedList
 }
--- go/armstrong_test.go
+++ go/armstrong_test.go
@@ -1,10 +1,31 @@
 package armstrong
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"container/list"
+	"reflect"
+)
 
 func TestArmstrongNumbers(t *testing.T) {
-	expected := 123
-	if observed := GenerateArmstrongNumbers(); observed != expected {
-		t.Fatalf("GenerateArmstrongNumbers() = %v, want %v", observed, expected)
+	expectedList := list.New()
+	expectedList.PushBack(153)
+	expectedList.PushBack(370)
+	expectedList.PushBack(371)
+	expectedList.PushBack(407)
+
+
+	if observed := GenerateArmstrongNumbers(); !reflect.DeepEqual(observed, expectedList) {
+		fmt.Println("expected list and observed list are different, however should be equal")
+		fmt.Println("\nexpected list: ")
+		for e := expectedList.Front(); e != nil; e = e.Next() {
+			fmt.Println(e)
+		}
+		fmt.Println("\nobserved list: ")
+		for e := observed.Front(); e != nil; e = e.Next() {
+			fmt.Println(e)
+		}
+		t.Fatalf("GenerateArmstrongNumbers() = %v, want %v", observed, expectedList)
+
 	}
 }
