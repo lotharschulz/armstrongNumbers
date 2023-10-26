3df369b69188a1eaf5c046d3afda376c19a0265d
Lothar Schulz <lothar.sch@gmx.de>
2023-10-26 10:45:27 +0200

    chore armstrong numbers implemented in golang

--- go/armstrong.go
+++ go/armstrong.go
@@ -20,6 +20,19 @@ func GenerateThreeDigitArmstrongNumbers() *list.List {
 	return expectedArmstrongNumberList
 }
 
+func GenerateThreeDigitArmstrongNumbersAlt() *list.List {
+	expectedArmstrongNumberList := list.New()
+
+	for i := 100; i < 1000; i++ {
+		if IsArmstrongNumberAlt(i) {
+			expectedArmstrongNumberList.PushBack(i)
+		}
+	}
+
+	return expectedArmstrongNumberList
+}
+
+
 // determines if a given number is an Armstrong number
 func IsArmstrongNumber(n int) bool {
 	if n < 0 {
@@ -27,36 +40,47 @@ func IsArmstrongNumber(n int) bool {
 	}
 	total := 0
 
-	// digits := GetDigits(n)
-	// numberOfDigits := GetNumberOfDigits(digits)
-	// for _, v := range digits {
-	// 	total = total + int(math.Pow(float64(v), float64(numberOfDigits)))
-	// }
-
-	// alternative implementation via string conversion
-	digitsAsRuneArray := GetDigitsViaStringConversation(n)
-	numberOfDigits := GetNumberOfDigitsFromeRuneArray(digitsAsRuneArray)
-	fmt.Println(digitsAsRuneArray)
-	// fmt.Println(numberOfDigits)
-	for _, v := range digitsAsRuneArray {
-		s := string(v)
-		// fmt.Print("converted to string: ")
-		// fmt.Println(s, reflect.TypeOf(s))
-
-		f, err := strconv.ParseFloat(s, 64)
-		if err != nil {
-			panic("Conversion to float failed")
-		}		   
-		// fmt.Print("converted to float: ")
-		// fmt.Println(f, err, reflect.TypeOf(f))
-
-		total = total + int(math.Pow(f, float64(numberOfDigits)))
+	digits := GetDigits(n)
+	numberOfDigits := GetNumberOfDigits(digits)
+	for _, v := range digits {
+		total = total + int(math.Pow(float64(v), float64(numberOfDigits)))
 	}
+
 	if total == n {
 		return true
 	}
 	return false
+}
 
+// determines if a given number is an Armstrong number
+func IsArmstrongNumberAlt(n int) bool {
+	if n < 0 {
+		return false
+	}
+	total := 0
+
+	// implementation via string conversion
+	digitsAsRuneArray := GetDigitsViaStringConversation(n)
+	numberOfDigits := GetNumberOfDigitsFromeRuneArray(digitsAsRuneArray)
+	for _, runeValue := range digitsAsRuneArray {
+		total = total + int(math.Pow(RuneToFloat(runeValue), float64(numberOfDigits)))
+	}
+
+	if total == n {
+		return true
+	}
+	return false
+}
+
+
+// returns a float64 value from a rune if conversion is applicable, else panic
+func RuneToFloat(r rune) float64 {
+	runeStringValue := string(r)
+	runeFloatValue, err := strconv.ParseFloat(runeStringValue, 64)
+	if err != nil {
+		panic("Conversion to float failed")
+	}
+	return runeFloatValue
 }
 
 // returns the digits with a given integer without string conversation
@@ -73,14 +97,7 @@ func GetDigits(n int) []int {
 // returns the digits with a given integer using string conversation
 func GetDigitsViaStringConversation(n int) []rune {
 	str := strconv.Itoa(n)
-	fmt.Println("str: " + str)
 	strRune := []rune(str)
-	for i := 0; i < len(strRune); i++ {
-        char := string(strRune[i])
-        fmt.Println(char, i)
-    }
-	fmt.Print("strRune: ")
-	fmt.Println(strRune)
 	return strRune
 }
 
@@ -98,19 +115,21 @@ func GetNumberOfDigits(digits []int) int {
 
 // returns the number of digits/integers of an rune array
 func GetNumberOfDigitsFromeRuneArray(digits []rune) int {
-	// for i := 0; i < len(digits); i++ {
-    //     char := string(digits[i])
-    //     fmt.Println(char, i)
-    // }
-	// fmt.Println(len(digits))
 	return len(digits)
 }
 
 // main function
 func main() {
-	fmt.Println("Armstrong numbers (last item in curly brackets)")
+	fmt.Println("Three digit armstrong numbers (last item in curly brackets): ")
 	armstrongNumbers := GenerateThreeDigitArmstrongNumbers()
 	for e := armstrongNumbers.Front(); e != nil; e = e.Next() {
 		fmt.Println(e)
 	}
+
+	fmt.Println("\n\nAlternative implementation\nThree digit armstrong numbers (last item in curly brackets): ")
+	armstrongNumbersAlt := GenerateThreeDigitArmstrongNumbersAlt()
+	for e := armstrongNumbersAlt.Front(); e != nil; e = e.Next() {
+		fmt.Println(e)
+	}
+
 }
